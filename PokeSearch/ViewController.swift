f1afb6781bbda4119e528272faa75fef979027bb
Richard Rodriguez <richardrodz@gmail.com>
2017-05-11 07:28:12 -0500

    Initial Commit

---
+++ PokeSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeSearch
+//
+//  Created by Richard Rodriguez on 5/11/17.
+//  Copyright © 2017 Richard Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
